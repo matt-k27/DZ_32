77ec42ee201d1f76d6f9668add3abfe8821446e9
Matt <matt@MacBook-Pro-Matt.local>
2024-04-09 18:32:14 +0200

    fraction

---
+++ fraction.py
@@ -0,0 +1,37 @@
+class Fraction:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __mul__(self, other):
+        if not isinstance(other, Fraction):
+            raise TypeError("Unsupported operand type")
+        return Fraction(self.a * other.a, self.b * other.b)
+
+    def __add__(self, other):
+        if not isinstance(other, Fraction):
+            raise TypeError("Unsupported operand type")
+        return Fraction(self.a * other.b + other.a * self.b, self.b * other.b)
+
+    def __sub__(self, other):
+        if not isinstance(other, Fraction):
+            raise TypeError("Unsupported operand type")
+        return Fraction(self.a * other.b - other.a * self.b, self.b * other.b)
+
+    def __eq__(self, other):
+        if not isinstance(other, Fraction):
+            return False
+        return self.a * other.b == other.a * self.b
+
+    def __gt__(self, other):
+        if not isinstance(other, Fraction):
+            raise TypeError("Unsupported operand type")
+        return self.a * other.b > other.a * self.b
+
+    def __lt__(self, other):
+        if not isinstance(other, Fraction):
+            raise TypeError("Unsupported operand type")
+        return self.a * other.b < other.a * self.b
+
+    def __str__(self):
+        return f"Fraction: {self.a}, {self.b}"
---
+++ main.py
@@ -0,0 +1,27 @@
+print('DZ32')
+
+from fraction import Fraction
+
+try:
+    testVar = Fraction(2, 3) * 5
+except TypeError as e:
+    print(e)
+
+f_a = Fraction(2, 3)
+f_b = Fraction(3, 6)
+f_c = f_b + f_a
+assert str(f_c) == 'Fraction: 21, 18'
+f_d = f_b * f_a
+assert str(f_d) == 'Fraction: 6, 18'
+f_e = f_a - f_b
+assert str(f_e) == 'Fraction: 3, 18'
+
+assert f_d < f_c  # True
+assert f_d > f_e  # True
+assert f_a != f_b  # True
+f_1 = Fraction(2, 4)
+f_2 = Fraction(3, 6)
+assert f_1 == f_2  # True
+print('OK')
+
+print('Thank you for using')
